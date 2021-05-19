ee02355f561aff5147804d0558f942e5f63ef671
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-05-19 13:25:17 +0800

    Add files via upload

--- nets/resnet.py
+++ nets/resnet.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 
 from keras import layers
+from keras.initializers import random_normal
 from keras.layers import (Activation, BatchNormalization, Conv2D,
                           Conv2DTranspose, Dropout, MaxPooling2D,
                           ZeroPadding2D)
@@ -17,15 +18,15 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a', use_bias=False)(input_tensor)
+    x = Conv2D(filters1, (1, 1), kernel_initializer=random_normal(stddev=0.02), name=conv_name_base + '2a', use_bias=False)(input_tensor)
     x = BatchNormalization(name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters2, kernel_size,padding='same', name=conv_name_base + '2b', use_bias=False)(x)
+    x = Conv2D(filters2, kernel_size,padding='same', kernel_initializer=random_normal(stddev=0.02), name=conv_name_base + '2b', use_bias=False)(x)
     x = BatchNormalization(name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c', use_bias=False)(x)
+    x = Conv2D(filters3, (1, 1), kernel_initializer=random_normal(stddev=0.02), name=conv_name_base + '2c', use_bias=False)(x)
     x = BatchNormalization(name=bn_name_base + '2c')(x)
 
     x = layers.add([x, input_tensor])
@@ -40,20 +41,20 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    x = Conv2D(filters1, (1, 1), strides=strides,
+    x = Conv2D(filters1, (1, 1), strides=strides, kernel_initializer=random_normal(stddev=0.02), 
                name=conv_name_base + '2a', use_bias=False)(input_tensor)
     x = BatchNormalization(name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters2, kernel_size, padding='same',
+    x = Conv2D(filters2, kernel_size, padding='same', kernel_initializer=random_normal(stddev=0.02), 
                name=conv_name_base + '2b', use_bias=False)(x)
     x = BatchNormalization(name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c', use_bias=False)(x)
+    x = Conv2D(filters3, (1, 1), kernel_initializer=random_normal(stddev=0.02), name=conv_name_base + '2c', use_bias=False)(x)
     x = BatchNormalization(name=bn_name_base + '2c')(x)
 
-    shortcut = Conv2D(filters3, (1, 1), strides=strides,
+    shortcut = Conv2D(filters3, (1, 1), strides=strides, kernel_initializer=random_normal(stddev=0.02), 
                       name=conv_name_base + '1', use_bias=False)(input_tensor)
     shortcut = BatchNormalization(name=bn_name_base + '1')(shortcut)
 
@@ -66,7 +67,7 @@ def ResNet50(inputs):
     # 512x512x3
     x = ZeroPadding2D((3, 3))(inputs)
     # 256,256,64
-    x = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', use_bias=False)(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2), kernel_initializer=random_normal(stddev=0.02), name='conv1', use_bias=False)(x)
     x = BatchNormalization(name='bn_conv1')(x)
     x = Activation('relu')(x)
 
@@ -115,20 +116,20 @@ def centernet_head(x,num_classes):
         x = Activation('relu')(x)
     # 最终获得128,128,64的特征层
     # hm header
-    y1 = Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer='he_normal', kernel_regularizer=l2(5e-4))(x)
+    y1 = Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer=random_normal(stddev=0.02), kernel_regularizer=l2(5e-4))(x)
     y1 = BatchNormalization()(y1)
     y1 = Activation('relu')(y1)
-    y1 = Conv2D(num_classes, 1, kernel_initializer='he_normal', kernel_regularizer=l2(5e-4), activation='sigmoid')(y1)
+    y1 = Conv2D(num_classes, 1, kernel_initializer=random_normal(stddev=0.02), kernel_regularizer=l2(5e-4), activation='sigmoid')(y1)
 
     # wh header
-    y2 = Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer='he_normal', kernel_regularizer=l2(5e-4))(x)
+    y2 = Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer=random_normal(stddev=0.02), kernel_regularizer=l2(5e-4))(x)
     y2 = BatchNormalization()(y2)
     y2 = Activation('relu')(y2)
-    y2 = Conv2D(2, 1, kernel_initializer='he_normal', kernel_regularizer=l2(5e-4))(y2)
+    y2 = Conv2D(2, 1, kernel_initializer=random_normal(stddev=0.02), kernel_regularizer=l2(5e-4))(y2)
 
     # reg header
-    y3 = Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer='he_normal', kernel_regularizer=l2(5e-4))(x)
+    y3 = Conv2D(64, 3, padding='same', use_bias=False, kernel_initializer=random_normal(stddev=0.02), kernel_regularizer=l2(5e-4))(x)
     y3 = BatchNormalization()(y3)
     y3 = Activation('relu')(y3)
-    y3 = Conv2D(2, 1, kernel_initializer='he_normal', kernel_regularizer=l2(5e-4))(y3)
+    y3 = Conv2D(2, 1, kernel_initializer=random_normal(stddev=0.02), kernel_regularizer=l2(5e-4))(y3)
     return y1, y2, y3
