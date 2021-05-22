55abb133d985567cd934ee15951a70a3f952e2e8
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-05-22 11:22:57 +0800

    Add files via upload

--- centernet.py
+++ centernet.py
@@ -1,6 +1,6 @@
 import colorsys
 import os
-
+import time
 import numpy as np
 from keras import backend as K
 from PIL import ImageDraw, ImageFont
@@ -25,9 +25,10 @@ class CenterNet(object):
     _defaults = {
         "model_path"        : 'model_data/centernet_resnet50_voc.h5',
         "classes_path"      : 'model_data/voc_classes.txt',
+        "backbone"          : 'resnet50',
         # "model_path"        : 'model_data/centernet_hourglass_coco.h5',
         # "classes_path"      : 'model_data/coco_classes.txt',
-        "backbone"          : 'resnet50',
+        # "backbone"          : 'hourglass',
         "input_shape"       : [512,512,3],
         "confidence"        : 0.3,
         # backbone为resnet50时建议设置为True
@@ -188,6 +189,72 @@ class CenterNet(object):
             draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
             del draw
         return image
+
+    def get_FPS(self, image, test_interval):
+        image_shape = np.array(np.shape(image)[0:2])
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #---------------------------------------------------------#
+        crop_img = letterbox_image(image, [self.input_shape[1], self.input_shape[0]])
+        #----------------------------------------------------------------------------------#
+        #   将RGB转化成BGR，这是因为原始的centernet_hourglass权值是使用BGR通道的图片训练的
+        #----------------------------------------------------------------------------------#
+        photo = np.array(crop_img,dtype = np.float32)[:,:,::-1]
+        #-----------------------------------------------------------#
+        #   图片预处理，归一化。获得的photo的shape为[1, 512, 512, 3]
+        #-----------------------------------------------------------#
+        photo = np.expand_dims(preprocess_image(photo), 0)
+
+        preds = self.centernet.predict(photo)
+        
+        if self.nms:
+            preds = np.array(nms(preds, self.nms_threhold))
+
+        if len(preds[0])>0:
+            #-----------------------------------------------------------#
+            #   将预测结果转换成小数的形式
+            #-----------------------------------------------------------#
+            preds[0][:, [0,2]] = preds[0][:, [0,2]] / (self.input_shape[1] / 4)
+            preds[0][:, [1,3]] = preds[0][:, [1,3]] / (self.input_shape[0] / 4)
+            
+            det_label   = preds[0][:, -1]
+            det_conf    = preds[0][:, -2]
+            det_xmin, det_ymin, det_xmax, det_ymax = preds[0][:, 0], preds[0][:, 1], preds[0][:, 2], preds[0][:, 3]
+
+            top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
+            top_conf = det_conf[top_indices]
+            top_label_indices = det_label[top_indices].tolist()
+            top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
+            
+            boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.input_shape[0],self.input_shape[1]]),image_shape)
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            preds = self.centernet.predict(photo)
+            
+            if self.nms:
+                preds = np.array(nms(preds, self.nms_threhold))
+
+            if len(preds[0])>0:
+                #-----------------------------------------------------------#
+                #   将预测结果转换成小数的形式
+                #-----------------------------------------------------------#
+                preds[0][:, [0,2]] = preds[0][:, [0,2]] / (self.input_shape[1] / 4)
+                preds[0][:, [1,3]] = preds[0][:, [1,3]] / (self.input_shape[0] / 4)
+                
+                det_label   = preds[0][:, -1]
+                det_conf    = preds[0][:, -2]
+                det_xmin, det_ymin, det_xmax, det_ymax = preds[0][:, 0], preds[0][:, 1], preds[0][:, 2], preds[0][:, 3]
+
+                top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
+                top_conf = det_conf[top_indices]
+                top_label_indices = det_label[top_indices].tolist()
+                top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
+                
+                boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.input_shape[0],self.input_shape[1]]),image_shape)
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
 
     def close_session(self):
         self.sess.close()
