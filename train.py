5c5de7d89a1ae790ca4cc794b5783e13219ce470
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-11-17 01:27:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -164,16 +164,16 @@ if __name__ == "__main__":
         start_epoch = Init_Epoch
         end_epoch   = Freeze_Epoch
 
-        model.compile(optimizer = Adam(lr), loss = {'centernet_loss': lambda y_true, y_pred: y_pred})
-
-        train_dataloader    = CenternetDatasets(train_lines, input_shape, batch_size, num_classes, train = True)
-        val_dataloader      = CenternetDatasets(val_lines, input_shape, batch_size, num_classes, train = False)
-
         epoch_step          = num_train // batch_size
         epoch_step_val      = num_val // batch_size
 
         if epoch_step == 0 or epoch_step_val == 0:
             raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
+
+        model.compile(optimizer = Adam(lr), loss = {'centernet_loss': lambda y_true, y_pred: y_pred})
+
+        train_dataloader    = CenternetDatasets(train_lines, input_shape, batch_size, num_classes, train = True)
+        val_dataloader      = CenternetDatasets(val_lines, input_shape, batch_size, num_classes, train = False)
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(
@@ -197,16 +197,16 @@ if __name__ == "__main__":
         start_epoch = Freeze_Epoch
         end_epoch   = UnFreeze_Epoch
 
-        model.compile(optimizer = Adam(lr), loss = {'centernet_loss': lambda y_true, y_pred: y_pred})
-
-        train_dataloader    = CenternetDatasets(train_lines, input_shape, batch_size, num_classes, train = True)
-        val_dataloader      = CenternetDatasets(val_lines, input_shape, batch_size, num_classes, train = False)
-
         epoch_step          = num_train // batch_size
         epoch_step_val      = num_val // batch_size
 
         if epoch_step == 0 or epoch_step_val == 0:
             raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
+
+        model.compile(optimizer = Adam(lr), loss = {'centernet_loss': lambda y_true, y_pred: y_pred})
+
+        train_dataloader    = CenternetDatasets(train_lines, input_shape, batch_size, num_classes, train = True)
+        val_dataloader      = CenternetDatasets(val_lines, input_shape, batch_size, num_classes, train = False)
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(
